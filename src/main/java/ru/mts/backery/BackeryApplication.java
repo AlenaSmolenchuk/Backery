59b00adb4ce03e1bc64df63ec3016e13f2757128
yelena.smolenchuk <helen.smolench@gmail.com>
2024-03-14 15:22:51 +0300

    start

---
+++ src/main/java/ru/mts/backery/BackeryApplication.java
@@ -0,0 +1,13 @@
+package ru.mts.backery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackeryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackeryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/mts/backery/BackeryApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mts.backery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackeryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
